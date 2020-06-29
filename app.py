c8d11622c5e0c6111b374665ece1c0971f0a60b3
Naresh <nareshh74@gmail.com>
2020-06-29 16:19:08 +0530

    placed app confifs in seperate file

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, send_from_directory, request
+from flask_restplus import Api, Resource
+from flask_jwt_extended import (jwt_required, get_jwt_identity, create_access_token, JWTManager)
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+# create flask app
+app = Flask(__name__)
+
+# configure flask app
+app.config.from_pyfile('config - dev.py', silent=True)
+
+# create extension instances
+api = Api(app, doc = '/docs')
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+# protected download API
+@api.route('/app/v1/resources/files/<path:path>')
+class File(Resource):
+    @jwt_required
+    def get(self, path):
+        current_user = get_jwt_identity
+        if not current_user:
+            return 'authentiation failed'
+        if not os.path.isfile(path):
+            return "invalid file/path"
+        return send_from_directory(app.root_path, path, as_attachment = True)
+
+# generate token
+@api.route('/app/v1/resources/tokens')
+class Token(Resource):
+    def get(self):
+        if not request.authorization:
+            return 'basic authentication needed'
+        if not request.authorization.username:
+            return 'provide username'
+        conn = db.engine.connect()
+        row = conn.execute("SELECT Name, Password FROM AppUser WHERE Name = '{u}'".format(u = "user1")).fetchone()
+        if not row:
+            return "invalid user"
+        if row[1] != request.authorization.password:
+            return "wrong password"
+        token = create_access_token(identity = request.authorization.username, expires_delta = datetime.timedelta(minutes = 2))
+        return token
+
+app.run()

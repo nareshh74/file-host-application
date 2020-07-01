26b08c229d461a4d26072a1d6bbc51ab52b81c25
Naresh <nareshh74@gmail.com>
2020-07-01 12:39:29 +0530

    Made db changes for my user management flask app POC, so updating it here too

--- config-dev.py
+++ config-dev.py
@@ -1,4 +1,4 @@
 DEBUG = True
 SECRET_KEY = "secret"
-SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://PRINHYLTPDL1561/api_dev?driver=SQL+Server?trusted_connection=yes"
+SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://PRINHYLTPDL1561/HumanResource?driver=SQL+Server?trusted_connection=yes"
 SQLALCHEMY_TRACK_MODIFICATIONS = False

f1ed0785fb6f68ac4cd21ac2880230bfaf16d002
Naresh <nareshh74@gmail.com>
2020-07-02 15:49:19 +0530

    test and file management related changes

--- config-dev.py
+++
@@ -1,4 +0,0 @@
-DEBUG = True
-SECRET_KEY = "secret"
-SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://PRINHYLTPDL1561/HumanResource?driver=SQL+Server?trusted_connection=yes"
-SQLALCHEMY_TRACK_MODIFICATIONS = False

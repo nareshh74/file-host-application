560cbb33ea4b5a744a38a7cc1fb5542a3e98d92b
Naresh <nareshh74@gmail.com>
2020-07-08 21:10:53 +0530

    error handling and server logs

--- application/auth.py
+++ application/auth.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, make_response, request
+from flask import Blueprint, make_response, request, abort, jsonify
 from flask_restplus import Api, Resource
 from flask_jwt_extended import create_access_token
 from flask_sqlalchemy import SQLAlchemy
 from . import extensions
+import logging
 
 auth_blueprint = Blueprint('auth', __name__)
 auth_api = Api(auth_blueprint)
 auth_namespace = auth_api.namespace('auth')
+auth_logger = logging.getLogger()
 
 @auth_namespace.route('/tokens')
 class Token(Resource):
@@ -17,13 +19,16 @@ class Token(Resource):
         conn = extensions.db.engine.connect()
         row = conn.execute("EXECUTE VerifyAppUser '{m}', '{p}'".format(m = request.authorization.username, p = request.authorization.password)).fetchone()
         if row['Column'] == 0:
-            return make_response("invalid user", 401)
+            abort(401)
         if row['Column'] == 1:
-            return make_response("wrong password", 401)
+            abort(401)
         token = create_access_token(identity = row['Column'], expires_delta = False)
         return {'token' : token}, 200
 
-@auth_namespace.route('/test')
-class Test(Resource):
-    def get(self):
-        return {"Test":"Test"}, 200
+@auth_blueprint.route('/test', methods=['GET'])
+def test():
+    return {"message":"API is UP!!"}, 200
+
+@auth_blueprint.route('/<path:wrong_path>', methods=['GET', 'POST'])
+def throw(wrong_path):
+    abort(404, request.method + ' to the route ' + str(wrong_path) + ' is not valid')
--- config.py
+++ config.py
@@ -1,7 +1,27 @@
 from os import environ
+from datetime import date
 
-class Config:
+class Config(object):
     SECRET_KEY = environ['APPLICATION_SECRET_KEY']
+    DOWNLOAD_FOLDER = '/application/host_files/01/'
+    LOGGING_CONFIG = {
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] {%(pathname)s:%(funcName)s:%(lineno)d} %(levelname)s - %(message)s',
+        }},
+        'handlers': {'default': {
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'C:\\Users\\nareshv\\Downloads\\Naresh\\01 - Programming\\projects\\file-host-application\\logs\\' + 'application - ' + str(date.today()) + '.log',
+            'maxBytes': 10000,
+            'backupCount': 10
+        }},
+        'root': {
+            'level': 'DEBUG',
+            'handlers': ['default']
+        }
+    }
 
 class Production_Config(Config):
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://application1:application1@PRINHYLTPDL1561/HumanResource?driver=SQL+Server"

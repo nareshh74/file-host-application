f1e2523a752c08e128e5eebee82fe1240fab92ac
Naresh <nareshh74@gmail.com>
2020-07-10 16:27:06 +0530

    do i/p validation using flask-restplus rather than custom decorators as it does both i/p validation and documentation

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,2 @@
-from file_host_application.lib.wrappers import authenticate_token, validate_requestjson
+from file_host_application.lib.wrappers import authenticate_token, handle_exception
 from file_host_application.lib.extensions import db, jwt
--- routes/files.py
+++ routes/files.py
@@ -1,39 +1,39 @@
 from flask import Blueprint, send_from_directory, current_app as app, abort, request, make_response
-from flask_restplus import Api, Resource
+from flask_restplus import Api, Resource, fields
 import os
 
-from file_host_application.lib import authenticate_token, validate_requestjson
+from file_host_application.lib import authenticate_token, handle_exception
 
 
 files_blueprint = Blueprint('files', __name__)
 files_api = Api(files_blueprint)
-files_namespace = files_api.namespace('files')
+files_namespace = files_api.namespace(name='files', description='API to handle file requests')
+file_version = files_namespace.model('File version', {'version':fields.Raw(required=True, description='File version of the requesting device', example='v1.0.0')})
 
 
 @files_namespace.route('/')
 class Files(Resource):
+    @files_namespace.header('Api-Key', description='Authentication token')
+    @files_namespace.header('Content-Type', description='serialization type - expected value = application/json')
+    @files_namespace.expect(file_version)
+    @files_namespace.response(200, '{"message":"up to date"}')
+    @files_namespace.produces('application/json')
     @authenticate_token
-    @validate_requestjson(['version', 'device'])
+    @handle_exception
     def get(self):
-        try:
-            latest_file_name = None
-            print(app.root_path + '\\' + app.config['FILES_FOLDER'])
-            for root, dirs, files in os.walk(app.root_path + '\\' + app.config['FILES_FOLDER']):
-                for file in files:
-                    if file.endswith(".onnx"):
-                        latest_file_name = file
-                        break
-            
-            if(request.json['version'] == os.path.splitext(latest_file_name)[0]):
-                return {'message':'up to date'}, 200
-
-            response = send_from_directory(app.root_path + app.config['FILES_FOLDER'], latest_file_name, as_attachment=True, conditional=True)
-            return response
+        latest_file_name = None
+        print(app.root_path + '\\' + app.config['FILES_FOLDER'])
+        for root, dirs, files in os.walk(app.root_path + '\\' + app.config['FILES_FOLDER']):
+            for file in files:
+                if file.endswith(".onnx"):
+                    latest_file_name = file
+                    break
         
-        except Exception as e:
-            message = getattr(e, 'message', repr(e))
-            code = getattr(e, 'code', 500)
-            abort(code, message)
+        if(request.json['version'] == os.path.splitext(latest_file_name)[0]):
+            return {'message':'up to date'}, 200
+
+        response = send_from_directory(app.root_path + app.config['FILES_FOLDER'], latest_file_name, as_attachment=True, conditional=True)
+        return response
 
 
 @files_blueprint.route('/test', methods=['GET'])

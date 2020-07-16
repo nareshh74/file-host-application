65eaf0dd2c2a36a168e8ede3e692b5a75ddcb086
Naresh <nareshh74@gmail.com>
2020-07-16 20:11:29 +0530

    test

--- file_host_application/__init__.py
+++ file_host_application/__init__.py
@@ -7,6 +7,7 @@ import atexit
 from werkzeug.contrib.fixers import ProxyFix
 from flask_cors import CORS
 
+
 from file_host_application.routes import files_blueprint, files_namespace
 from file_host_application.config import Development, Config, Production
 

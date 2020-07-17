11f7e1bce5ee1074493a84838ff13cc1e4edf20e
Naresh <nareshh74@gmail.com>
2020-07-17 20:23:18 +0530

    comments

--- file_host_application/routes/files.py
+++ file_host_application/routes/files.py
@@ -1,16 +1,20 @@
 # controller - /files
+# 3rd party imports
 from flask import Blueprint, send_from_directory, current_app as app, abort, request, make_response, redirect, url_for
 from flask_restplus import Api, Resource
 import os
 
+# internal imports
 from file_host_application.lib import handle_exception, authenticate_token, authorizations
 
 
+# bluprint level variables
 files_blueprint = Blueprint('files', __name__)
 files_api = Api(files_blueprint)
 files_namespace = files_api.namespace(name='files', description='API to handle file requests', authorizations=authorizations)
 
 
+# endpoint definitions
 @files_namespace.route('/<string:file_version>', endpoint='version_check')
 class FileVersion(Resource):
     @authenticate_token
@@ -23,15 +27,20 @@ class FileVersion(Resource):
     @files_namespace.doc(responses={'303':'Redirect to the route /files. Please refer /files route response documentation'})
     @handle_exception
     def get(self, file_version):
+
+        # find first .onnx file in FILES_FOLDER
         latest_file_name = None
         for root, dirs, files in os.walk(app.root_path + '/' + app.config['FILES_FOLDER']):
             for file in files:
                 if file.endswith(".onnx"):
                     latest_file_name = file
                     break
+        
+        # return if up to date
         if(file_version == os.path.splitext(latest_file_name)[0]):
             return {'message':'up to date'}, 200
         
+        # redirect to file download if not
         redirect_url = url_for('files.file_download', latest_file_name=latest_file_name)
         response = make_response(redirect(redirect_url), 303)
         return response
@@ -48,12 +57,16 @@ class FileName(Resource):
     @handle_exception
     def get(self):
         latest_file_name = request.args.get('latest_file_name', None)
+
+        # find first .onnx file in FILES_FOLDER
         if latest_file_name is None:
             for root, dirs, files in os.walk(app.root_path + '/' + app.config['FILES_FOLDER']):
                 for file in files:
                     if file.endswith(".onnx"):
                         latest_file_name = file
                         break
+        
+        # attachment for file download, conditional for range requests
         response = send_from_directory(app.root_path + '/' + app.config['FILES_FOLDER'], latest_file_name, as_attachment=True, conditional=True)
         return response
 
